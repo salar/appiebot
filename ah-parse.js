624cc2c2aaec5d5749d4536f65af2afa346beda4
Salar al Khafaji <code@salar.me>
2020-04-09 12:51:58 +0200

    Clean up and nicer notification messages

--- ah-parse.js
+++ ah-parse.js
@@ -1,7 +1,5 @@
 const request = require("request-promise")
 
-// let data = require("./example-data/ah-2.json")
-// let data = require("./example-data/ah-all-full.json")
 const daysOfWeek = ["zondag", "maandag", "dinsdag", "woensdag", "donderdag", "vrijdag", "zaterdag"]
 const months = ["jan", "feb", "maart", "april", "mei", "juni", "juli", "aug", "sep", "okt", "nov", "dec"]
 
@@ -25,19 +23,6 @@ function processAHResult(result)
     }
 }
 
-function processResult(error, response, body)
-{
-    if (error)
-    {
-        process.stdout.write("Error!")
-        process.stdout.write(response)
-        process.stdout.write(body)
-        console.log(error)
-    }
-    else
-        process.stdout.write(presentAvailability(processAHResult(body)))
-}
-
 function prettyPrintDate(date)
 {
     let d = new Date(date)
@@ -49,18 +34,19 @@ function prettyJoin(s)
     return s.length > 1 ? s.slice(0, s.length - 1).join(", ") + " en " + s[s.length - 1] : s[0]
 }
 
-exports.presentAvailability = function presentAvailability(slots)
+exports.presentAvailability = function presentAvailability(slots, prevSlotsLen)
 {
-    return slots.length
-        ? "Nieuwe slots beschikbaar op " + prettyJoin(slots.map(prettyPrintDate)) + "!"
-        : "Geen slots beschikbaar."
+    if (!slots.length)
+        return "Geen slots beschikbaar."
+    
+    const avail = prettyJoin(slots.map(prettyPrintDate))
+    if (slots.length < prevSlotsLen)
+        return "Nu alleen nog op " + avail
+    else if (prevSlotsLen === 0)
+        return "Nieuwe slots beschikbaar op " + avail + "!"
+    else
+        return "Nu ook op " + avail
 }
-
-// process.stdout.write("Hello!")
-// process.stdout.write(presentAvailability(processAHResult(data)))
-
-// let url = "https://www.ah.nl/servicxe/rest/delegate?url=%2Fkies-moment%2Fbezorgen%2F1052AE"
-// request({ url: url, json: true }, processResult)
 
 exports.getAvailability = async function getAvailability(postcode)
 {
